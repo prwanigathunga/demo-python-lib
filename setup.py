efb1155a6244fb896a5b6b7d4906b4a41e46ba58
Ranga Wanigathunga <prwanigathunga@gmail.com>
2024-03-24 11:52:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cloudrangalabs1",
+    name="cloudquicklabs1",
     version="0.0.1",
     author="Ranga Wanigathunga",
     author_email="prwanigathunga@gmail.com",
